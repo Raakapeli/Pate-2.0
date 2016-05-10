9ed665bbff794614fb7aad7bdcd2955ffb93a380
Raakapeli <joninis@metropolia.fi>
2016-05-10 09:02:43 +0300

    Update Touchsensor.java

--- Pate 2.0/src/features/Touchsensor.java
+++ Pate 2.0/src/features/Touchsensor.java
@@ -1,30 +1,19 @@
 package features;
 
-import lejos.hardware.Button;
-import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3TouchSensor;
 
-public class Touchsensor {
+public class Touchsensor{
 	
-	private EV3TouchSensor touchsensor;
-	int touchCount = 100;
-	int pushed = 0;
-
-	int sampleSize = touchsensor.sampleSize();
-	float[] sample = new float[sampleSize];
-    
-	public Touchsensor(EV3TouchSensor sensor) {
-		this.touchsensor = sensor;
+	private EV3TouchSensor tSensor;
+	
+	public Touchsensor(EV3TouchSensor sensor){
+		this.tSensor = sensor;
+		
+	}
+	public float[] getTouched(){
+		float[] touch = new float[tSensor.sampleSize()];
+		tSensor.fetchSample(touch, 0);
+		return touch;	
 	}
 	
-	public int getTouched(){
-    	while ((touchCount > 0) && (!Button.ESCAPE.isDown())){	
-    		do{
-    			touchsensor.fetchSample(sample, 0);
-    		}
-    		while (sample[0] == 0);
-				pushed = 1;
-    		}
-		return this.pushed;	
-    }
 }
