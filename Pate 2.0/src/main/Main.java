fabc2af96766351c13e2320898ddfe5849856038
Raakapeli <joninis@metropolia.fi>
2016-05-10 09:01:47 +0300

    Update Main.java

--- Pate 2.0/src/main/Main.java
+++ Pate 2.0/src/main/Main.java
@@ -5,11 +5,14 @@ import lejos.hardware.lcd.LCD;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.port.MotorPort;
 import lejos.hardware.port.SensorPort;
+import lejos.hardware.sensor.EV3ColorSensor;
 import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.utility.Delay;
 import features.IRSCheckerForRemote;
-
+import features.Colorsensor;
+import features.Motors;
+import features.Touchsensor;
 
 public class Main{
 	
@@ -17,105 +20,145 @@ public class Main{
 		
 		EV3LargeRegulatedMotor leftMotor = new EV3LargeRegulatedMotor(MotorPort.A);
 		EV3LargeRegulatedMotor rightMotor = new EV3LargeRegulatedMotor(MotorPort.B);
-		EV3LargeRegulatedMotor righthand = new EV3LargeRegulatedMotor(MotorPort.C);
-		EV3LargeRegulatedMotor lefthand = new EV3LargeRegulatedMotor(MotorPort.D);
+		EV3LargeRegulatedMotor rightHand = new EV3LargeRegulatedMotor(MotorPort.C);
+		EV3LargeRegulatedMotor leftHand = new EV3LargeRegulatedMotor(MotorPort.D);
+		Motors leftTrack = new Motors(leftMotor);
+		Motors rightTrack = new Motors(rightMotor);
+		Motors leftCircularsaw = new Motors(leftHand);
+		Motors rightCircularsaw = new Motors(rightHand);
+		
 		EV3IRSensor irSensor = new EV3IRSensor(SensorPort.S4);
 		IRSCheckerForRemote remoteChecker = new IRSCheckerForRemote(irSensor);
-		EV3TouchSensor ts = new EV3TouchSensor(SensorPort.S1);
+		
+		EV3TouchSensor tSensor = new EV3TouchSensor(SensorPort.S1);
+		Touchsensor ts = new Touchsensor(tSensor);
+		
+		EV3ColorSensor ambientmode = new EV3ColorSensor(SensorPort.S3);
+		Colorsensor lightsensor = new Colorsensor(ambientmode);
 		
 		remoteChecker.start();		
-		String leftMotorStatus = "Vasen seis";
-		String rightMotorStatus = "Oikea seis";
-		float[] sample = new float[ts.sampleSize()];
-		
+		float[] touch = new float[tSensor.sampleSize()];
+//		float[] ambient = new float[ambientmode.sampleSize()];
+//		double ambient = ambientmode.fetchsample(ambient,0);
+		String emergency = "EMERGENCY STOP";
+//		String sun = "Sun is shining!!";
+//		if(ambient > 0.2){
+//			LCD.clear();
+//			LCD.drawString(sun, 0, 1);
+//		}
+		Button.LEDPattern(0);
+		LCD.clear();
 		while (!Button.ESCAPE.isDown()){
 			int cmd = remoteChecker.getRemoteCmd();
-			ts.fetchSample(sample, 0);
-			
+			touch = ts.getTouched();
+			lightsensor.getAmbientValue();
 			switch (cmd){
 				case 3: //oikea eteen, vasen seis
-		        leftMotor.stop(true);
-		        rightMotor.stop();
-		        rightMotor.forward();
+				LCD.clear();
+				Button.LEDPattern(0);
+		        leftTrack.motorStop();
+		        rightTrack.motorStop();
+		        rightTrack.motorForward();
 		        break;
 				case 1: //vasen eteen, oikea seis
-		        rightMotor.stop(true);
-		        leftMotor.stop();
-		        leftMotor.forward();
+				LCD.clear();
+				Button.LEDPattern(0);
+		        rightTrack.motorStop();
+		        leftTrack.motorStop();
+		        leftTrack.motorForward();
 		        break;
 				case 4: //oikea taakse, vasen seis
-			    leftMotor.stop(true);
-			    rightMotor.stop();
-			    rightMotor.backward();
+				LCD.clear();
+				Button.LEDPattern(0);
+			    leftTrack.motorStop();
+			    rightTrack.motorStop();
+			    rightTrack.motorBackward();
 			    break;
 				case 2: //vasen taakse, oikea seis
-			    rightMotor.stop(true);
-			    leftMotor.stop();
-			    leftMotor.backward();
+				LCD.clear();	
+				Button.LEDPattern(0);
+			    rightTrack.motorStop();
+			    leftTrack.motorStop();
+			    leftTrack.motorBackward();
 			    break;
 				case 6: //oikea taakse, vasen eteen
-				leftMotor.stop(true);
-				leftMotor.forward();
-				rightMotor.stop();
-				rightMotor.backward();
+				LCD.clear();
+				Button.LEDPattern(0);
+				leftTrack.motorStop();
+				leftTrack.motorForward();
+				rightTrack.motorStop();
+				rightTrack.motorBackward();
 				break;
 				case 7: //vasen taakse, oikea eteen
-				rightMotor.stop(true);
-				rightMotor.forward();
-				leftMotor.stop();
-				leftMotor.backward();
+				LCD.clear();
+				Button.LEDPattern(0);
+				rightTrack.motorStop();
+				rightTrack.motorForward();
+				leftTrack.motorStop();
+				leftTrack.motorBackward();
 				break;
 				case 5:  //molemmat eteen
-		        leftMotor.stop(true);
-		        rightMotor.stop();
-		        leftMotor.forward();
-		        rightMotor.forward();
+				LCD.clear();
+				Button.LEDPattern(0);
+		        leftTrack.motorStop();
+		        rightTrack.motorStop();
+		        leftTrack.motorForward();
+		        rightTrack.motorForward();
 		        break;
 				case 8:  //molemmat taakse
-		        leftMotor.stop(true);
-		        rightMotor.stop();
-		        leftMotor.backward();
-		        rightMotor.backward();
+				LCD.clear();
+				Button.LEDPattern(0);
+		        leftTrack.motorStop();
+		        rightTrack.motorStop();
+		        leftTrack.motorBackward();
+		        rightTrack.motorBackward();
 		        break;
-				case 9: //stop, oli case 9
-		        leftMotor.stop(true);
-		        rightMotor.stop();
+				case 9: //stop
+				LCD.clear();
+				Button.LEDPattern(0);
+		        leftTrack.motorStop();
+		        rightTrack.motorStop();
 		        break;
 				case 10:
-				lefthand.stop(true);
-				righthand.stop();
-				lefthand.forward();
-				righthand.forward();
+				LCD.clear();
+				Button.LEDPattern(0);
+				leftCircularsaw.motorStop();
+				rightCircularsaw.motorStop();
+				leftCircularsaw.motorForward();
+				rightCircularsaw.motorForward();
 				break;
 				case 11:
-				lefthand.stop(true);
-				righthand.stop();
+				LCD.clear();
+				Button.LEDPattern(0);
+				leftCircularsaw.motorStop();
+				rightCircularsaw.motorStop();
 				break;
 				}				
-					LCD.drawString(leftMotorStatus, 0, 1);
-					LCD.drawString(rightMotorStatus, 0, 2);
-					
-				if (sample[0] == 1){
-					leftMotor.stop();
-					rightMotor.stop();
-					lefthand.stop();
-					righthand.stop();
+				
+				if (touch[0] == 1){
+					Button.LEDPattern(8);
+					LCD.clear();
+					LCD.drawString(emergency, 0, 3);
+					leftCircularsaw.motorStop();
+					rightCircularsaw.motorStop();
+					leftTrack.motorStop();
+					rightTrack.motorStop();
 					Delay.msDelay(500);
-					leftMotor.backward();
-					rightMotor.backward();
-					Delay.msDelay(1000);
-					leftMotor.stop();
-					rightMotor.stop();
+					leftTrack.motorBackward();
+					rightTrack.motorBackward();
+					Delay.msDelay(3000);
+					leftTrack.motorStop();
+					rightTrack.motorStop();
 					
 					Delay.msDelay(500);
-					rightMotor.stop(true);
-					rightMotor.forward();
-					leftMotor.stop();
-					leftMotor.backward();
-					Delay.msDelay(1000);
+//					leftTrack.motorStop();      //Vaihdoin paikkaa
+//					rightTrack.motorStop();		//			
+					rightTrack.motorForward();
+					leftTrack.motorBackward();
+					Delay.msDelay(2000);
 					
-					leftMotor.stop();
-					rightMotor.stop();
+					leftTrack.motorStop();
+					rightTrack.motorStop();
 				} else {
 					continue;				
 				}
@@ -124,10 +167,13 @@ public class Main{
 			remoteChecker.stopSampling();
 			leftMotor.close();
 			rightMotor.close();
-			righthand.close();
-			lefthand.close();
-			ts.close();
+			rightHand.close();
+			leftHand.close();
+			tSensor.close();
+			irSensor.close();
+			ambientmode.close();
 		}
+		
 	}
 
 
