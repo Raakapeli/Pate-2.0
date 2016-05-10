b62b6403f53657cedfeda9dd49e2403dbc56609a
Raakapeli <joninis@metropolia.fi>
2016-05-10 09:03:05 +0300

    Update Motors.java

--- Pate 2.0/src/features/Motors.java
+++ Pate 2.0/src/features/Motors.java
@@ -1,5 +1,21 @@
 package features;
 
+import lejos.hardware.motor.EV3LargeRegulatedMotor;
+
 public class Motors {
 	
+	private EV3LargeRegulatedMotor motor;
+	
+	public Motors(EV3LargeRegulatedMotor motor){
+		this.motor = motor;
+	}
+	public void motorForward(){
+		motor.forward();
+	}
+	public void motorBackward(){
+		motor.backward();
+	}
+	public void motorStop(){
+		motor.stop();
+	}
 }
