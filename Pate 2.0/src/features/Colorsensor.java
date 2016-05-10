5ccf46897057fd45eb867cc97ff67b273f5fc65b
Raakapeli <joninis@metropolia.fi>
2016-05-10 13:22:32 +0300

    Update Colorsensor.java

--- Pate 2.0/src/features/Colorsensor.java
+++ Pate 2.0/src/features/Colorsensor.java
@@ -1,21 +1,18 @@
 package features;
 
-import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3ColorSensor;
-import lejos.hardware.sensor.SensorMode;
 
 public class Colorsensor{
 	
 	private EV3ColorSensor cSensor;
-	//SensorMode ambientmode = cSensor.getAmbientMode();
+	
 	
 	public Colorsensor(EV3ColorSensor sensor){
 		this.cSensor = sensor;
 	}
-	public float[] getAmbientValue(){
-		float[] ambient = new float[cSensor.sampleSize()];
-		cSensor.fetchSample(ambient, 0);
-		return ambient;
+	public int getColorValue(){
+		int color = cSensor.getColorID();
+		return color;
 	}
 	
 	
