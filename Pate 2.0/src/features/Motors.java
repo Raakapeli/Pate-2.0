f14eae7c159c34a283fe163ceedf2e116a35bb97
jrvq <jarvinen.joonass@gmail.com>
2016-05-11 09:54:08 +0300

    Update Motors.java
    added java docs

--- Pate 2.0/src/features/Motors.java
+++ Pate 2.0/src/features/Motors.java
@@ -8,23 +8,58 @@ import lejos.hardware.sensor.EV3IRSensor;
 import lejos.hardware.sensor.EV3TouchSensor;
 import features.IRSCheckerForRemote;
 
+/**
+ * Motor object
+ * @author Joonas Järvinen
+ * @author Joni Niskasaari
+ * @author Juho Ranta
+ * @author Riku Isola 
+ * @version 2.0 10.5.2016
+ */
 public class Motors {
 	
 	private EV3LargeRegulatedMotor motor;
 	
+	/**
+	 * Motor objects constructor
+	 * @param motor
+	 */
 	public Motors(EV3LargeRegulatedMotor motor){
 		this.motor = motor;
 	}
+	
+	/**
+	 * Moves the motor forward
+	 */
 	public void motorForward(){
 		motor.forward();
 	}
+	
+	/**
+	 * Moves motor backward
+	 */
 	public void motorBackward(){
 		motor.backward();
 	}
+	
+	/**
+	 * Stops motor
+	 */
 	public void motorStop(){
 		motor.stop();
 	}
 	
+	/**
+	 * Stop all the motors and sensors                         POISTA!!!! JOS EI KÄYTETÄ!!!!
+	 * @param remCheck											
+	 * @param leftM															^
+	 * @param rightM													   / \
+	 * @param rightH														|
+	 * @param leftH															|
+	 * @param tSen															|
+	 * @param irSen
+	 * @param ambientM
+	 */
 	public static void stopMotor(IRSCheckerForRemote remCheck, EV3LargeRegulatedMotor leftM,
 			EV3LargeRegulatedMotor rightM, EV3LargeRegulatedMotor rightH, EV3LargeRegulatedMotor leftH,
 			EV3TouchSensor tSen, EV3IRSensor irSen, EV3ColorSensor ambientM) {
@@ -39,9 +74,12 @@ public class Motors {
 		ambientM.close();		
 	}
 	
+	/**
+	 * Moves left motor forward
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void leftMotorForward(Motors leftT, Motors rightT){
-		
-
 		LCD.clear();
 		Button.LEDPattern(0);
         leftT.motorStop();
@@ -49,6 +87,11 @@ public class Motors {
         leftT.motorForward();
 	}
 	
+	/**
+	 * Moves right motor forward
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void rightMotorForward(Motors leftT, Motors rightT){
 		LCD.clear();
 		Button.LEDPattern(0);
@@ -57,6 +100,11 @@ public class Motors {
         rightT.motorForward();
 	}
 	
+	/**
+	 * Moves right motor backward
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void rightMotorBackward(Motors leftT, Motors rightT){
 		LCD.clear();
 		Button.LEDPattern(0);
@@ -65,6 +113,11 @@ public class Motors {
 	    rightT.motorBackward();		
 	}
 	
+	/**
+	 * Moves left motor backward
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void leftMotorBackward(Motors leftT, Motors rightT){
 		LCD.clear();	
 		Button.LEDPattern(0);
@@ -73,6 +126,11 @@ public class Motors {
 	    leftT.motorBackward();
 	}
 	
+	/**
+	 * Moves right motor backwards and left motor forward
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void rightBackwardLeftForward(Motors leftT, Motors rightT){
 		LCD.clear();
 		Button.LEDPattern(0);
@@ -81,6 +139,12 @@ public class Motors {
 		rightT.motorStop();
 		rightT.motorBackward();
 	}
+	
+	/**
+	 * Moves right motor forward and left motor backward
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void rightForwardLeftBackward(Motors leftT, Motors rightT){
 		LCD.clear();
 		Button.LEDPattern(0);
@@ -89,6 +153,12 @@ public class Motors {
 		rightT.motorForward();
 		leftT.motorBackward();
 	}
+	
+	/**
+	 * Moves right and left motor forward
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void rightForwardLeftForward(Motors leftT, Motors rightT){
 		LCD.clear();
 		Button.LEDPattern(0);
@@ -97,6 +167,12 @@ public class Motors {
         leftT.motorForward();
         rightT.motorForward();
 	}
+	
+	/**
+	 * Moves right and left motor backward
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void rightBackwardLeftBackward(Motors leftT, Motors rightT){
 		LCD.clear();
 		Button.LEDPattern(0);
@@ -105,12 +181,24 @@ public class Motors {
         leftT.motorBackward();
         rightT.motorBackward();
 	}
+	
+	/**
+	 * Stops both left and right motor
+	 * @param leftT
+	 * @param rightT
+	 */
 	public static void rightStopLeftStop(Motors leftT, Motors rightT){
 		LCD.clear();
 		Button.LEDPattern(0);
         leftT.motorStop();
         rightT.motorStop();
 	}
+	
+	/**
+	 * Starts to rotate both circular saws
+	 * @param leftH
+	 * @param rightH
+	 */
 	public static void SawsOn(Motors leftH, Motors rightH){
 		LCD.clear();
 		Button.LEDPattern(0);
@@ -119,6 +207,12 @@ public class Motors {
 		leftH.motorForward();
 		rightH.motorForward();
 	}
+	
+	/**
+	 * Stops both circular saws
+	 * @param leftH
+	 * @param rightH
+	 */
 	public static void SawsOff(Motors leftH, Motors rightH){
 		LCD.clear();
 		Button.LEDPattern(0);
@@ -126,11 +220,3 @@ public class Motors {
 		rightH.motorStop();
 	}
 }
-
-
-
-
-
-
-
-
