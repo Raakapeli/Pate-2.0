687af9317e9c4a893369134321ce772a401db79f
jrvq <jarvinen.joonass@gmail.com>
2016-05-11 09:55:04 +0300

    Update Touchsensor.java
    java doc added

--- Pate 2.0/src/features/Touchsensor.java
+++ Pate 2.0/src/features/Touchsensor.java
@@ -5,21 +5,45 @@ import lejos.hardware.lcd.LCD;
 import lejos.hardware.sensor.EV3TouchSensor;
 import lejos.utility.Delay;
 
+/**
+ * Creates object for touch sensor
+ * @author Joonas Järvinen
+ * @author Joni Niskasaari
+ * @author Juho Ranta
+ * @author Riku Isola 
+ * @version 2.0 10.5.2016
+ */
 public class Touchsensor{
 	
 	private EV3TouchSensor tSensor;
 	
+	/**
+	 * Touch sensor constructor
+	 * @param sensor
+	 */
 	public Touchsensor(EV3TouchSensor sensor){
 		this.tSensor = sensor;
 		
 	}
 
+	/**
+	 *  ??    TARVIIKO TÄTÄ    ??
+	 * @return
+	 */
 	public float[] getTouched(){
 		float[] touch = new float[tSensor.sampleSize()];
 		tSensor.fetchSample(touch, 0);
 		return touch;	
 	}
 	
+	/**
+	 * When the touch sensor is pressed the robot will go backwards and then turn another way
+	 * @param vasentrack
+	 * @param oikeatrack
+	 * @param vasensaha
+	 * @param oikeasaha
+	 * @param emer
+	 */
 	public static void goBackward(Motors vasentrack, Motors oikeatrack, Motors vasensaha, Motors oikeasaha, String emer){
 		Button.LEDPattern(8);
 		LCD.clear();
